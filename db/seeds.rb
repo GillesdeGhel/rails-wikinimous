e9be30bc2995175ac5b4b16cfa2a75ecac03ff0c
Gilles de Ghellinck <gillesdg95@gmail.com>
2018-10-30 14:45:16 +0100

    it aint easy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+names = []
+10.times do
+  names << Faker::LeagueOfLegends.quote
+end
+
+content = []
+10.times do
+  content << Faker::Lorem.paragraphs
+end
+
+10.times do
+  Article.create(title: names.sample, content: content.sample)
+end
